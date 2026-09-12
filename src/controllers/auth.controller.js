39ec36dafbdf61e5712c186ea65bfbf5990dc939
Sauravc-Dev <SAURAVKHANAL842@GMAIL.COM>
2026-09-12 23:04:38 +0545

    feat:read jwt token and return user on the basis of that

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import userModel from "../models/user.model.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+export async function register(req,res){
+    const {username,email,password}=req.body;
+    const isAlreadyregistered=await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isAlreadyregistered){
+        res.status(409).json({
+            message:"username or email already exist"
+        })
+    }
+    const hashedPassword=crypto.createHash("sha256").update(password).digest("hex");
+    const user=await userModel.create({
+        username,
+        email,
+        password:hashedPassword
+    })
+    const token=jwt.sign({
+        id:user._id
+    },config.JWT_SECRET,{
+        expiresIn:"1d"
+    })
+    res.status(201).json({
+        message:"user registeres succesfully",
+        user:{
+            username:user.username,
+            email:user.email,
+        },
+        token
+    })
+}
+
+export async function getMe(req,res) {
+    const token=req.headers.authorization?.split(" ")[1];
+    if(!token){
+        return res.status(401).json({
+            message:"token not found"
+        })
+    }
+    const decoded=jwt.verify(token,config.JWT_SECRET)
+    const user=await userModel.findById(decoded.id)
+    res.status(200).json({
+        message:"user fetched succesfully",
+        user:{
+            username:user.username,
+            email:user.email,
+        }
+    })
+}
